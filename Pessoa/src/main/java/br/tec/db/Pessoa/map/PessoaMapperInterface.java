02d8df337e82ef65730e303acec30d53462038d7
renansalves <renan.alves@edu.pucrs.br>
2025-10-20 16:47:14 -0300

    Feat: Calcular idade
    Implemnta o calculo de idade com base na data de nascimento.

--- Pessoa/src/main/java/br/tec/db/Pessoa/map/PessoaMapperInterface.java
+++ Pessoa/src/main/java/br/tec/db/Pessoa/map/PessoaMapperInterface.java
@@ -1,6 +1,10 @@
 package br.tec.db.Pessoa.map;
 
+import java.time.LocalDate;
+import java.time.Period;
+
 import org.mapstruct.Mapper;
+import org.mapstruct.Named;
 
 import br.tec.db.Pessoa.dto.PessoaDto;
 import br.tec.db.Pessoa.modelo.Pessoa;
@@ -15,4 +19,11 @@ public interface PessoaMapperInterface {
 
   Pessoa toEntity(PessoaDto pessoaDto);
 
+  @Named("calcularIdade")
+  public static Integer calcularIdade(LocalDate dataNascimento) {
+    if (dataNascimento == null) {
+      return null; // Retorna nulo se a data de nascimento for nula
+    }
+    return Period.between(dataNascimento, LocalDate.now()).getYears();
+  }
 }
